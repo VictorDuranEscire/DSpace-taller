b3afba5b1943abda15d0fc5c6d9494df11780672
VictorHugoDS13 <innovacionescire@outlook.com>
2024-07-24 09:39:40 -0600

    Generador de PDF(obsoleto)

--- dspace-api/src/main/java/org/dspace/app/mediafilter/PDFBoxDocument.java
+++ dspace-api/src/main/java/org/dspace/app/mediafilter/PDFBoxDocument.java
@@ -1,10 +1,12 @@
 package org.dspace.app.mediafilter;
 
+import com.google.common.collect.Lists;
 import org.apache.logging.log4j.Logger;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.dspace.authorize.AuthorizeException;
 import org.dspace.content.Bitstream;
 import org.dspace.content.Bundle;
 import org.dspace.content.Item;
@@ -14,17 +16,19 @@ import org.dspace.content.service.ItemService;
 import org.dspace.core.Context;
 import org.dspace.storage.bitstore.DSBitStoreService;
 import org.dspace.utils.DSpace;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
+
+import java.io.*;
+
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 
-public class PDFBoxDocument extends ImageMagickThumbnailFilter{
+public class PDFBoxDocument extends MediaFilter {
 
     private static Logger log = org.apache.logging.log4j.LogManager.getLogger(PDFBoxThumbnail.class);
     protected final List<String> publicFiltersClasses = new ArrayList<>();
@@ -48,7 +52,7 @@ public class PDFBoxDocument extends ImageMagickThumbnailFilter{
 
     @Override
     public String getFormatString() {
-        return "PDF";
+        return "Adobe PDF";
     }
 
     @Override
@@ -89,46 +93,68 @@ public class PDFBoxDocument extends ImageMagickThumbnailFilter{
     public InputStream getDestinationStream(Item item, InputStream source, boolean verbose) throws Exception {
         Context context = new Context();
 
-        Optional<Bundle> bundle = item.getBundles().stream().filter( bundle1 -> bundle1.getName().contains("ORIGINAL") ).findFirst();
+        Optional<Bundle> bundle = item.getBundles().stream().filter( bundle1 -> bundle1.getName().contains("ORIGINAL")  ).findFirst();
 
         if (bundle.isEmpty()) {
             throw new Exception("Could not find ORIGINAL bundle");
         }
 
+        System.out.println("Procesando InputStream");
         PDDocument document = new PDDocument();
+           List<InputStream>  inputStreamsList = bundle.get().getBitstreams().stream().filter(bitstream1 -> !bitstream1.getName().equals("OUTPUT.pdf")).map(
+                   bitstream -> {
+                       try {
+                           return bitstreamService.retrieve(context, bitstream);
+                       } catch (IOException | SQLException | AuthorizeException e) {
+                           throw new RuntimeException(e);
+                       }
+                   }
+           ).collect(Collectors.toList());
 
-        for (Bitstream bitstream : bundle.get().getBitstreams()) {
+        System.out.println("Procesando imagenes");
+
+        List<List<InputStream>> lists =  Lists.partition(inputStreamsList,5);
+
+        for (List<InputStream> inputStreams: lists) {
             try {
-                final InputStream inputStream = bitstreamService.retrieve(context, bitstream);
+               for (InputStream inputStream: inputStreams) {
+                   try {
+                       PDPage page = new PDPage();
+                       page.setMediaBox(PDRectangle.A4);
+                       ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                       byte[] buffer = new byte[8192]; // Tamaño del buffer, puedes ajustarlo según tus necesidades
+                       int bytesRead;
+                       while ((bytesRead = inputStream.read(buffer)) != -1) {
+                           outputStream.write(buffer, 0, bytesRead);
+                       }
+                       byte[] byteArray = outputStream.toByteArray();
 
+                       PDImageXObject image = PDImageXObject.createFromByteArray(document,byteArray, "image");
+                       boolean isBigger = image.getWidth() >= 2482;
+                       float width = PDRectangle.A4.getWidth();
+                       float height = PDRectangle.A4.getHeight();
+                       if(isBigger){
+                           page.setMediaBox(new PDRectangle(2383.937F, 2070.3938F));
+                           width =  PDRectangle.A0.getWidth();
+                           height = 2070;
+                       }
+                       if(image.getWidth() <= 500) {
+                           page.setMediaBox(new PDRectangle(image.getWidth(), image.getHeight()));
+                           width =  image.getWidth();
+                           height = image.getHeight();
+                       }
+                       // Añadir la imagen a la página
+                       try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
+                           contentStream.drawImage(image, 0, 0, width,height);
+                           contentStream.close();
+                       }
+                       document.addPage(page);
+                       outputStream.close();
+                   } finally {
+                       inputStream.close();
 
-                try {
-                    PDPage page = new PDPage();
-                    page.setMediaBox(PDRectangle.A4);
-
-                    // Convertir BufferedImage a PDImageXImage
-                    PDImageXObject image = PDImageXObject.createFromByteArray(document, inputStream.readAllBytes(), "image");
-                    boolean isBigger = image.getWidth() >= 2482;
-                    float width = PDRectangle.A4.getWidth();
-                    float height = PDRectangle.A4.getHeight();
-                    if(isBigger){
-                        page.setMediaBox(new PDRectangle(2383.937F, 2070.3938F));
-                         width =  PDRectangle.A0.getWidth();
-                         height = 2070;
-                    }
-                    if(image.getWidth() <= 500) {
-                        page.setMediaBox(new PDRectangle(image.getWidth(), image.getHeight()));
-                        width =  image.getWidth();
-                        height = image.getHeight();
-                    }
-                    // Añadir la imagen a la página
-                    try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
-                        contentStream.drawImage(image, 0, 0, width,height);
-                    }
-                    document.addPage(page);
-                } finally {
-                    inputStream.close();
-                }
+                   }
+               }
 
             } catch (Exception e){
                 e.printStackTrace();
